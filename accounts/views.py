5090606d9506f25caf1e09a3cfc312ca0403e8c4
manny <ebrahimi_manny@yahoo.com>
2025-05-14 18:06:10 +0330

    added user registration API

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer
 
 # Create your views here.
+class RegisterView(APIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+
+            return Response(
+                {
+                "user": serializer.data,
+                "message": "User created successfully",
+                "token": token.key
+                },
+                status=status.HTTP_201_CREATED
+            )
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        return Response({"message": "Welcome to the Chatman API"}, status=status.HTTP_200_OK)
